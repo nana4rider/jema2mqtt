6512567cbde2599c7bb544dabc0de618da770d0b
Shunichiro Aki <register.develop@nana4.net>
2025-01-01 10:55:31 +0900

    feat: add cover

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ type Entity = {
   monitorGpio: number;
 };
 
-type EntityDomain = "lock" | "switch";
+type EntityDomain = "lock" | "switch" | "cover";
 
 const TopicType = {
   COMMAND: "set",
@@ -54,7 +54,12 @@ async function main() {
     const baseMessage = {
       unique_id: `jema2mqtt_${deviceId}_${entity.id}`,
       name: entity.name,
+      command_topic: getTopic(entity, TopicType.COMMAND),
+      state_topic: getTopic(entity, TopicType.STATE),
+      availability_topic: getTopic(entity, TopicType.AVAILABILITY),
       optimistic: false,
+      qos,
+      retain: true,
       device: {
         identifiers: [`jema2mqtt_${deviceId}`],
         name: `jema2mqtt.${deviceId}`,
@@ -69,18 +74,29 @@ async function main() {
     };
     const { domain } = entity;
 
-    if (domain === "lock" || domain === "switch") {
+    if (domain === "lock") {
       return {
         ...baseMessage,
-        command_topic: getTopic(entity, TopicType.COMMAND),
-        state_topic: getTopic(entity, TopicType.STATE),
-        availability_topic: getTopic(entity, TopicType.AVAILABILITY),
         payload_lock: StatusMessage.ACTIVE,
         payload_unlock: StatusMessage.INACTIVE,
         state_locked: StatusMessage.ACTIVE,
         state_unlocked: StatusMessage.INACTIVE,
-        qos,
-        retain: true,
+      };
+    } else if (domain === "switch") {
+      return {
+        ...baseMessage,
+        payload_on: StatusMessage.ACTIVE,
+        payload_off: StatusMessage.INACTIVE,
+        state_on: StatusMessage.ACTIVE,
+        state_off: StatusMessage.INACTIVE,
+      };
+    } else if (domain === "cover") {
+      return {
+        ...baseMessage,
+        payload_close: StatusMessage.ACTIVE,
+        payload_open: StatusMessage.INACTIVE,
+        state_closed: StatusMessage.ACTIVE,
+        state_open: StatusMessage.INACTIVE,
       };
     }
 
