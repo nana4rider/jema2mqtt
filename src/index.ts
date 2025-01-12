874d4f599699eff707a26c1fe924cd2586afd0d3
Shunichiro Aki <register.develop@nana4.net>
2025-01-12 09:50:10 +0900

    feat: テストコード拡充

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import { Entity } from "@/entity";
 import logger from "@/logger";
+import { setupAvailability } from "@/manager/availabilityManager";
 import setupMqttDeviceManager from "@/manager/mqttDeviceManager";
-import { getTopic, TopicType } from "@/payload/topic";
 import initializeHttpServer from "@/service/http";
 import requestJemaAccess from "@/service/jema";
-import env from "env-var";
 import fs from "fs/promises";
 
 type Config = {
   deviceId: string;
   entities: Entity[];
 };
-
-const AVAILABILITY_INTERVAL = env
-  .get("AVAILABILITY_INTERVAL")
-  .default(10000)
-  .asIntPositive();
 
 async function main() {
   logger.info("start");
@@ -33,25 +27,11 @@ async function main() {
   );
   const mqtt = await setupMqttDeviceManager(deviceId, entities, jemas);
   const http = await initializeHttpServer();
-
-  http.setEndpoint("/health", () => ({}));
-
-  const publishAvailability = (value: string) => {
-    entities.forEach((entity) =>
-      mqtt.publish(getTopic(entity, TopicType.AVAILABILITY), value),
-    );
-  };
-
-  // オンライン状態を定期的に送信
-  const availabilityTimerId = setInterval(
-    () => void publishAvailability("online"),
-    AVAILABILITY_INTERVAL,
-  );
+  const availability = setupAvailability(entities, mqtt);
 
   const shutdownHandler = async () => {
     logger.info("shutdown");
-    clearInterval(availabilityTimerId);
-    publishAvailability("offline");
+    availability.close();
     await mqtt.close(true);
     await http.close();
     await Promise.all(Array.from(jemas.values()).map((jema) => jema.close()));
@@ -61,7 +41,7 @@ async function main() {
   process.on("SIGINT", () => void shutdownHandler());
   process.on("SIGTERM", () => void shutdownHandler());
 
-  publishAvailability("online");
+  availability.pushOnline();
 
   logger.info("ready");
 }
