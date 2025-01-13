f024323ddba9e57a6118635ee62ee595ed01fd1d
Shunichiro Aki <register.develop@nana4.net>
2025-01-14 05:18:44 +0900

    feat: 説明文を改善

--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,28 @@
-import { cleanEnv, num, str } from "envalid";
+import { cleanEnv, num, port, str } from "envalid";
 
 const env = cleanEnv(process.env, {
-  // MQTT設定
-  MQTT_BROKER: str(),
-  MQTT_USERNAME: str({ default: undefined }),
-  MQTT_PASSWORD: str({ default: undefined }),
-  MQTT_TASK_INTERVAL: num({ default: 100 }),
-  ENTITY_QOS: num({ choices: [0, 1, 2], default: 1 }),
-  // ログ出力
-  LOG_LEVEL: str({ default: "info" }),
-  // https://www.home-assistant.io/integrations/mqtt/#discovery-options
-  HA_DISCOVERY_PREFIX: str({ default: "homeassistant" }),
-  // HTTPサーバーのポート
-  PORT: num({ default: 3000 }),
-  // オンライン状態を送信する間隔
-  AVAILABILITY_INTERVAL: num({ default: 10000 }),
+  MQTT_BROKER: str({ desc: "MQTTブローカー", example: "mqtt://localhost" }),
+  MQTT_USERNAME: str({ desc: "MQTTユーザ名", default: undefined }),
+  MQTT_PASSWORD: str({ desc: "MQTTパスワード", default: undefined }),
+  MQTT_TASK_INTERVAL: num({ desc: "MQTTタスク実行間隔", default: 100 }),
+  ENTITY_QOS: num({
+    desc: "エンティティのQOS設定",
+    choices: [0, 1, 2],
+    default: 1,
+  }),
+  LOG_LEVEL: str({ desc: "ログレベル", default: "info" }),
+  HA_DISCOVERY_PREFIX: str({
+    desc: "https://www.home-assistant.io/integrations/mqtt/#discovery-options",
+    default: "homeassistant",
+  }),
+  PORT: port({
+    desc: "HTTPサーバーのポート",
+    default: 3000,
+  }),
+  AVAILABILITY_INTERVAL: num({
+    desc: "オンライン状態を送信する間隔",
+    default: 10000,
+  }),
 });
 
 export default env;
