5a61d3d2af24e8ebb0af2b89131032b9bc8663a6
nana4rider <register.develop@nana4.net>
2025-12-01 08:56:13 +0900

    feat: gpioコマンドの実行にexecFileを使う

--- tests/service/gpio.spec.ts
+++ tests/service/gpio.spec.ts
@@ -5,12 +5,13 @@ vi.mock("child_process", async () => {
   const actual = await vi.importActual<typeof child_process>("child_process");
   return {
     ...actual,
-    exec: vi.fn(),
+    execFile: vi.fn(),
   };
 });
 
-type ExecCallbackOnly = (
+type ExecFileCallbackOnly = (
   command: string,
+  args: string[],
   callback: (
     error: Error | null,
     result: { stdout: string; stderr: string },
@@ -24,47 +25,59 @@ beforeEach(() => {
 
 describe("read", () => {
   test("gpiogetコマンドが正しく設定される", async () => {
-    const mockExec = vi.mocked(child_process.exec as ExecCallbackOnly);
-    mockExec.mockImplementation((_command, callback) => {
+    const mockExecFile = vi.mocked(
+      child_process.execFile as ExecFileCallbackOnly,
+    );
+    mockExecFile.mockImplementation((_command, _args, callback) => {
       callback(null, { stdout: "1\n", stderr: "" });
       return {} as child_process.ChildProcess;
     });
 
     const value = await getGPIOValue(99);
 
-    vi.mocked(child_process.exec);
-
     expect(value).toBe(1);
-    expect(mockExec).toHaveBeenCalledTimes(1);
-    expect(mockExec.mock.calls[0][0]).toBe("gpioget --numeric --chip 0 99");
+    expect(mockExecFile).toHaveBeenCalledTimes(1);
+    expect(mockExecFile.mock.calls[0][0]).toBe("gpioget");
+    expect(mockExecFile.mock.calls[0][1]).toEqual([
+      "--numeric",
+      "--chip",
+      "0",
+      "99",
+    ]);
   });
 
   test("gpiogetコマンドが0|1以外の値を返したとき例外をスローする", async () => {
-    const mockExec = vi.mocked(child_process.exec as ExecCallbackOnly);
-    mockExec.mockImplementation((_command, callback) => {
-      callback(null, { stdout: "\n", stderr: "" });
+    const mockExecFile = vi.mocked(
+      child_process.execFile as ExecFileCallbackOnly,
+    );
+    mockExecFile.mockImplementation((_command, _args, callback) => {
+      callback(null, { stdout: "x\n", stderr: "" });
       return {} as child_process.ChildProcess;
     });
-
     const actual = getGPIOValue(99);
 
     await expect(actual).rejects.toThrowError();
   });
 
   test("gpiosetコマンドが正しく設定される", async () => {
-    const mockExec = vi.mocked(child_process.exec as ExecCallbackOnly);
-    mockExec.mockImplementation((_command, callback) => {
+    const mockExecFile = vi.mocked(
+      child_process.execFile as ExecFileCallbackOnly,
+    );
+    mockExecFile.mockImplementation((_command, _args, callback) => {
       callback(null, { stdout: "1\n", stderr: "" });
       return {} as child_process.ChildProcess;
     });
 
     await setGPIOValue(98, 1, { toggle: "250ms,0" });
 
-    vi.mocked(child_process.exec);
-
-    expect(mockExec).toHaveBeenCalledTimes(1);
-    expect(mockExec.mock.calls[0][0]).toBe(
-      "gpioset --chip 0 --toggle 250ms,0 98=1",
-    );
+    expect(mockExecFile).toHaveBeenCalledTimes(1);
+    expect(mockExecFile.mock.calls[0][0]).toBe("gpioset");
+    expect(mockExecFile.mock.calls[0][1]).toEqual([
+      "--chip",
+      "0",
+      "--toggle",
+      "250ms,0",
+      "98=1",
+    ]);
   });
 });
